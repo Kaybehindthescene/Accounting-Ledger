d052f9f115d0bcf4b54aaa880a7852ee97c0149d
CalebShufford <cshufford@my.yearupunited.org>
2025-10-14 22:41:59 -0400

    Created a test file that helps me see my TransactionsFile

--- src/main/java/com/pluralsight/TestLedger.java
+++ src/main/java/com/pluralsight/TestLedger.java
@@ -1,4 +1,32 @@
 package com.pluralsight;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.io.IOException;
+
 public class TestLedger {
+    public static void main(String[] args) throws IOException {
+        Path filePath = Path.of("transactions.csv");   // relative to project root
+        TransactionsFile file = new TransactionsFile(filePath);
+
+        Transaction newTransaction = new Transaction(
+                java.time.LocalDate.now(),
+                java.time.LocalTime.now(),
+                "Deposit",
+                "Direct Payroll",
+                new java.math.BigDecimal("1500.00")
+        );
+
+        file.append(newTransaction);
+        System.out.println("✅ Added new transaction!");
+
+
+        List<Transaction> all = file.loadAll();
+
+        // 3) print to verify
+        for (Transaction tr : all) {
+            System.out.println(tr.toCsvLine());
+        }
+    }
 }
+
